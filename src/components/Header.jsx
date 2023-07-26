96acb9ab9e0cb6527888dab794911a36696af63f
Yohan Velasquez <yohan.0055@gmail.com>
2023-07-26 02:56:27 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import NuevoPresupuesto from "./NuevoPresupuesto";
+import ControlPresupuesto from "./ControlPresupuesto";
+
+const Header = ({
+    gastos, presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto, setGastos
+}) => {
+    return (
+        <header>
+            <h1>Planificador de Gastos</h1>
+
+            {isValidPresupuesto ? (
+                <ControlPresupuesto
+                    setGastos={setGastos}
+                    gastos={gastos}
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+
+                />
+            ) : (
+                <NuevoPresupuesto
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
+            )};
+
+        </header>
+    );
+}
+
+export default Header;
